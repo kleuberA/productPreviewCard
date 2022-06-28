7c3f889f204a0f4720ec05ebcf3550f9ae1cace7
Kleuber Silva <kleuber20@hotmail.com>
2022-06-28 17:27:03 -0300

    Update Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="containerProduct">
+        <div className="ImageProduct"></div>
+        <div className="textProduct">
+          <span className="textPerfume">PERFUME</span>
+          <h1 className="titleNameProduct">Gabrielle Essence Eau De Parfum</h1>
+          <p className="descritionProduct">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+          <div className="prices">
+            <span className="pricePrincipal">$149.99</span>
+            <span className="priceAntigo">$169.99</span>
+          </div>
+          <button className="addToCart">Add to Cart</button>
+        </div>
+      </div>
     </div>
   );
 }
